853694047bbcf3456fa5c4802db616209940cc40
Brooke Huang <43490656+brookehuang@users.noreply.github.com>
2018-11-10 17:01:25 -0500

    Add files via upload
    powerball() function will automatically create 5 white numbers and 1 red numbers for you. Good luck!

---
+++ powerball_game.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#### small function to choose number for your powerball game!
+def powerball():
+    num1=random.randint(1,69)
+    num2=random.randint(1,69)
+    num3=random.randint(1,69)
+    num4=random.randint(1,69)
+    num5=random.randint(1,69)
+    red_num=random.randint(1,26)
+    print(num1,num2,num3,num4,num5,red_num)
+
